8bda280e140d7a1db55ba273bb87f8ee707f5ed2
Cesar Palma Aguilar <czarv10@gmail.com>
2025-02-26 17:39:28 -0500

    Fixed typo in db username. GetStudents now successfully returns all students from the database.

--- go_server.go
+++ go_server.go
@@ -19,16 +19,33 @@ func (h GetStudentsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// syntax above: "h" is like self in python class definition. The stuff in parenthesis attaches the function to the struct as a method. In go, structs can have methods.
 	// "w" is an instance of the http.ResponseWriter type
 	// Likewise with r but in this case is a pointer, as required by the package.
-	fmt.Fprintf(w, "Hello!")
+
+	fmt.Fprintf(w, "Hello! Pulling up all students ")
+
+	rows, _ := h.db.Query("SELECT first_name FROM students")
+	defer rows.Close()
+
+	var names []string
+
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		names = append(names, name)
+	}
+	fmt.Fprintf(w, "Students: %v", names)
+
 }
 
 func main() {
 
-	db, _ := sql.Open("postgres", "user=postgress password=dbzsuper dbname=school_records sslmode=disable")
+	db, err := sql.Open("postgres", "user=postgres password=dbzsuper dbname=school_records sslmode=disable")
 	// first argument is the driver name, hence postgres.
 	// Normally I wouldn't write a password straight into the code like this but secrets management feels
 	// out of scope for the assignment.
 
+	if err != nil || db == nil {
+		panic("Failed to connect to database: " + err.Error())
+	}
 	defer db.Close()
 	handler := GetStudentsHandler{db: db}
 	http.Handle("/GetStudents", handler)
